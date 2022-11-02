cb223bdd3ed30185d4f96557fddae6b169cce828
nurken <xteclu@gmail.com>
2022-11-02 16:09:51 +0600

    package

---
+++ Tests/QboxWebWidgetTests/QboxWebWidgetTests.swift
@@ -0,0 +1,10 @@
+import XCTest
+@testable import QboxWebWidget
+
+final class QboxWebWidgetTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+    }
+}
